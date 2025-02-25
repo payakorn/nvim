384f1ee99b937899c0c487b1920476a131f89c52
Payakorn-MacbookPro <payakorn.s@cmu.ac.th>
2025-02-25 21:59:29 +0100

    add new theme and transparent true

---
+++ lua/plugins/vesper.lua
@@ -0,0 +1,14 @@
+return {
+  -- add gruvbox
+  { "datsfilipe/vesper.nvim", opts = {
+    transparent = true,
+  } },
+
+  -- Configure LazyVim to load gruvbox
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "vesper",
+  --   },
+  -- },
+}
