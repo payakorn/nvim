0d4189a3614cff5724ce6780d2969658cf7ae543
Payakorn-MacbookPro <payakorn.s@cmu.ac.th>
2025-03-01 23:33:33 +0100

    add snippet and code suggestions

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,8 +1,10 @@
 return {
   "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
   opts = {
     transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
   },
 }
