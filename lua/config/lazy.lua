94e1c11b80233ff38cc2b452b28cae6cffc46b49
Payakorn-MacbookPro <payakorn.s@cmu.ac.th>
2025-08-01 10:41:32 +0200

    update packages

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,8 +17,17 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- { import = "lazyvim.plugins.extras.linting.eslint" },
+    {
+      "LazyVim/LazyVim",
+      import = "lazyvim.plugins",
+      opts = {
+        colorscheme = "catppuccin",
+        -- colorscheme = "solarized-osaka-moon",
+      },
+    },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- import/override with your plugins
     { import = "plugins" },
